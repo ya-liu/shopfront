6526ee3d22b9892c2ca1136b88636f06f160a5d6
Ya Liu <sarahl0216@gmail.com>
2021-11-26 18:50:48 -0800

    refactor express server to ts file, added examples

---
+++ server/index.ts
@@ -0,0 +1,53 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from the server!' });
+});
+
+interface LocationWithTimezone {
+  location: string;
+  timezoneName: string;
+  timezoneAbbr: string;
+  utcOffset: number;
+};
+
+const getLocationsWithTimezones = (request: Request, response: Response, next: NextFunction) => {
+  let locations: LocationWithTimezone[] = [
+    {
+      location: 'Germany',
+      timezoneName: 'Central European Time',
+      timezoneAbbr: 'CET',
+      utcOffset: 1
+    },
+    {
+      location: 'China',
+      timezoneName: 'China Standard Time',
+      timezoneAbbr: 'CST',
+      utcOffset: 8
+    },
+    {
+      location: 'Argentina',
+      timezoneName: 'Argentina Time',
+      timezoneAbbr: 'ART',
+      utcOffset: -3
+    },
+    {
+      location: 'Japan',
+      timezoneName: 'Japan Standard Time',
+      timezoneAbbr: 'JST',
+      utcOffset: 9
+    }
+  ];
+
+  response.status(200).json(locations);
+};
+
+app.get('/timezones', getLocationsWithTimezones);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
