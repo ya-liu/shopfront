a66207bcea8d9ec0cf1143a1b5ac426ab0ca75d4
Ya Liu <sarahl0216@gmail.com>
2021-11-29 18:23:08 -0800

    install react router and types for front-end routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,6 @@ function App() {
   const [message, setMessage] = useState<Message | undefined>(undefined);
 
   useEffect(() => {
-    // console.log(process.env.REACT_APP_NOT_SECRET_CODE);
     fetch(`/api`)
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
