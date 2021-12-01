8238c2e984b0c884607f8851fc80fa31abbd319a
Ya Liu <sarahl0216@gmail.com>
2021-11-30 21:33:07 -0800

    add interfaces for shopify api data

--- client/src/interfaces.ts
+++ client/src/interfaces.ts
@@ -3,3 +3,74 @@ export interface dummyProduct {
   name: string;
 }
 
+export interface ShopifyProduct {
+  id: number;
+  title: string;
+  body_html: string;
+  vendor: string;
+  product_type: string;
+  created_at: string;
+  handle: string;
+  updated_at: string;
+  published_at: string;
+  template_suffix: string;
+  status: string;
+  published_scope: string;
+  tags: string;
+  admin_graphql_api_id: string;
+  variants: Variant[];
+  options: Option[];
+  images: Image[];
+  image: Image;
+}
+
+export interface Variant {
+  id: number;
+  product_id: number;
+  title: string;
+  price: string;
+  sku: string;
+  position: number;
+  inventory_policy: string;
+  compare_at_price: string | null;
+  fulfillment_service: string;
+  inventory_management: string;
+  option1: string;
+  option2: string | null;
+  option3: string | null;
+  created_at: string;
+  updated_at: string;
+  taxable: boolean;
+  barcode: string;
+  grams: number;
+  image_id: number | null;
+  weight: number;
+  weight_unit: string;
+  inventory_item_id: number;
+  inventory_quantity: number;
+  old_inventory_quantity: number;
+  requires_shipping: boolean;
+  admin_graphql_api_id: string;
+}
+
+export interface Option {
+  id: number;
+  product_id: number;
+  name: string;
+  position: number;
+  values: string[]
+}
+
+export interface Image {
+  id: number;
+  product_id: number;
+  position: number;
+  created_at: string;
+  updated_at: string;
+  alt: string | null;
+  width: number;
+  height: number;
+  src: string;
+  variant_id: number[];
+  admin_graphql_api_id: string;
+}
--- src/controllers/shopify.ts
+++ src/controllers/shopify.ts
@@ -10,7 +10,7 @@ export const getProducts = async (req: Request, res: Response): Promise<void> =>
   try {
     const response = await axios.get(`https://${username}:${password}@${shop}.myshopify.com/admin/api/${apiversion}/${resource}.json`)
 
-    res.status(200).json(response.data);
+    res.status(200).json(response.data.products);
   } catch (error) {
     console.error(error);
   }
