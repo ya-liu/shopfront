1e9e90d1bd206dd3fd95b90c5809db3be7c90b6e
Ya Liu <sarahl0216@gmail.com>
2021-12-03 14:46:12 -0800

    refactored with MUI

--- client/src/routes/About.tsx
+++ client/src/routes/About.tsx
@@ -1,15 +1,18 @@
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 export default function About() {
   return (
     <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-      <h2>About</h2>
-      <p>
+      <Typography component="h2" variant="h4" align="center" gutterBottom>
+        About
+      </Typography>
+      <Typography component="p" variant="body1" align="center" gutterBottom>
         I am a software engineer, and I can't live without espresso. So I created this website to make sure espresso aficionados like me always have great beans.
-      </p>
-      <p>
-        Only the best espresso coffee beans here! You know you need them!
-      </p>
+      </Typography>
+      <Typography component="p" variant="body1" align="center" gutterBottom>
+        Only the best espresso coffee beans here! You know you need them! 🤓
+      </Typography>
     </Container>
   );
 }
