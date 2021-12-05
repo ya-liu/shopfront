969803787bbc6b850fe9b9963ec129cf9a1fbd8a
Ya Liu <sarahl0216@gmail.com>
2021-12-04 17:15:49 -0800

    modifying for deployment

--- src/controllers/stripe.ts
+++ src/controllers/stripe.ts
@@ -1,15 +1,16 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-const stripe = require('stripe')(process.env.stripeKey);
+// const stripe = require('stripe')(process.env.stripeKey);
 
-export const stripeSample = async () => {
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: 1000,
-    currency: 'usd',
-    payment_method_types: ['card'],
-    receipt_email: 'jenny.rosen@example.com',
-  });
+// export const stripeSample = async () => {
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: 1000,
+//     currency: 'usd',
+//     payment_method_types: ['card'],
+//     receipt_email: 'jenny.rosen@example.com',
+//   });
 
-  return paymentIntent;
-}
+//   return paymentIntent;
+// }
+export default {};
